3495115d6a0fa287e925f966c459546198391920
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-08 13:44:35 -0500

    added manufacturer funcatility to search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MakeService } from '../services/manufacter-services/make.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
 
+export class SearchComponent implements OnInit {
+
+  protected makeList: any;
+  protected manufature: string = "";
+  protected model: string = "";
+  constructor(private makeService: MakeService) {
+
+  }
+
+  ngOnInit(): void {
+    this.makeService.getAllManufactures().subscribe((manufaturers: any) => {
+      this.makeList = manufaturers.data;
+    })
+
+  }
 }
