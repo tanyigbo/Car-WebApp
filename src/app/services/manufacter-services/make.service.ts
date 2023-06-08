e88ba786714745e2cfa69880ec4c4be0221db487
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-08 13:43:10 -0500

    added service for getting makes

---
+++ src/app/services/manufacter-services/make.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MakeService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllManufactures() {
+    return this.http.get(`http://localhost:8080/api/manufacturers`);
+  }
+}
