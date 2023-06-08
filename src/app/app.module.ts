073bd65504074d902fe380e470cc040efc22e584
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-08 13:46:17 -0500

    added material ui forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialUiModule } from './material-ui/material-ui.module';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialUiModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material-ui/material-ui.module.ts
+++ src/app/material-ui/material-ui.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+
 
 @NgModule({
     declarations: [
@@ -13,11 +18,19 @@ import { MatButtonModule } from '@angular/material/button';
         MatInputModule,
         MatToolbarModule,
         MatButtonModule,
+        MatIconModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatSelectModule,
     ],
     exports: [
         MatInputModule,
         MatToolbarModule,
         MatButtonModule,
+        MatIconModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatSelectModule,
     ]
 })
 
