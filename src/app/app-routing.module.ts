3b6213204656e2c8e5df32cf246dc4faae10ad13
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-09 07:27:14 -0500

    added review routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarComponent } from './car/car.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ReviewComponent } from './review/review.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: ':model',
     component: CarComponent
+  },
+  {
+    path:':model/:id',
+    component: ReviewComponent
   },
   {
     path: '**',
