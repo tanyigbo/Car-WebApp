6a280cbc70eb6dfb99eb5dbd22e1c4f212103ead
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-09 03:36:17 -0500

    added get method for get car by model

--- src/app/services/car-services/car.service.ts
+++ src/app/services/car-services/car.service.ts
@@ -8,4 +8,7 @@ export class CarService {
 
   constructor(private http: HttpClient) { }
 
+  getCarByModel(model: string) {
+    return this.http.get(`http://localhost:8080/api/cars/model/${model}`);
+  }
 }
