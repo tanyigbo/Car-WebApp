ee6e763610da5c6ccbef4c67986ea01af661ea3a
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-09 03:35:18 -0500

    cars component displays images

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CarService } from '../services/car-services/car.service';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.css']
 })
-export class CarComponent {
+export class CarComponent implements OnInit {
+
+  protected modelName: any;
+  protected car: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private carService: CarService,
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((details: any) => {
+      this.modelName = details.model;
+      this.getCarInfo()
+    })    
+  }
+
+  getCarInfo(): void {
+    this.carService.getCarByModel(this.modelName.replace(" ", "_"))
+      .subscribe((data: any) => {
+        this.car = data.data;        
+      })
+  }
 
 }
