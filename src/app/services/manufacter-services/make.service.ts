d9fe31e91809a54a3e9b5aad8c5663b036c3022d
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-09 08:23:30 -0500

    services doc strings

--- src/app/services/manufacter-services/make.service.ts
+++ src/app/services/manufacter-services/make.service.ts
@@ -8,11 +8,20 @@ export class MakeService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * Uses API GET Request to retrive a list of all manufacturers
+   * @returns List of manufacturers as an observable
+   */
   getAllManufactures() {
     return this.http.get(`http://localhost:8080/api/manufacturers`);
   }
 
-  getAllCarsByManufacturer(make:string){
+  /**
+   * Uses API GET Request to retrive all cars by manufacturer
+   * @param make name of manufacturer
+   * @returns List of cars with matching manufacturer
+   */
+  getAllCarsByManufacturer(make: string) {
     return this.http.get(`http://localhost:8080/api/manufacturers/${make}/cars`);
   }
 }
