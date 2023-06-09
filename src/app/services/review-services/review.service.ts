fb4f783cafb3fe2d69e5db18cf6b64747127733e
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-09 07:38:28 -0500

    added get method for review by id

--- src/app/services/review-services/review.service.ts
+++ src/app/services/review-services/review.service.ts
@@ -8,7 +8,7 @@ export class ReviewService {
 
   constructor(private http:HttpClient) { }
 
-  getAllManufactures() {
-    return this.http.get(`http://localhost:8080/api/manufacturers`);
+  getReviewById(id:string) {
+    return this.http.get(`http://localhost:8080/api/reviews/${id}`);
   }
 }
