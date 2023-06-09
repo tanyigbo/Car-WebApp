b20b6b694d3270bfa7dbbdb62181aad416a4801a
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-06-09 06:48:07 -0500

    added all cars to homepage with liks to pages

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { MakeService } from '../services/manufacter-services/make.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
 
+  protected makeList:any;
+
+  constructor(private makeService:MakeService){}
+
+  ngOnInit(): void {
+    this.makeService.getAllManufactures().subscribe((makes:any)=>{
+      this.makeList = makes.data;     
+    });
+  }
 }
